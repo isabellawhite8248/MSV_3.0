e0a63a797a970b0e6155b65ad3838cd20a4ecbbd
isabellawhite8248 <97011021+isabellawhite8248@users.noreply.github.com>
2023-07-04 16:51:50 -0400

    Create Visualizer.java

---
+++ Visualizer.java
@@ -0,0 +1,21 @@
+public class Visualizer {
+
+    //contains a back button to animation screen
+    //carries out the animation at specific time intervals with specific dot groups
+    //also contains an exit progra button
+    //scrubbable time progress buttton with a pause play button, plays selected audio, dot arrays in the right place
+    //scrubbing can be a workable feature - don't implement right away
+
+    //after you are done with that class - go back - comment and reorganize code, write up the final read me and push to git with the previous msv versions
+    //upload live line up and plot out simple animations for each and animate them yeahhhh bitch you finished
+    //after you code them learn each on the guitar - then piano after yiruma kiss the rain - then singing then production
+
+    //after you have all these down - record an acoustic and musically produced version of each
+    //edit the videos - finish + brand your company with a website
+    //finish both websites
+    //finish leet and cracking the coding interview
+
+    //you'll have both brands launched -- work on writing your own music and figuring out a live setup - rehearse in the dorm room - then look for some open mics and start performing
+    //but upload one video once a week - either a pre recorded one, OG music or a popular cover and try to go network at open mics or find a gig a week
+
+}
